06d682d610729c46dd16b5bf0aed7ba1942522d8
Anna <hhermione@yandex.ru>
2023-07-27 22:51:45 +0500

    Поправила опечатку в инициализации цвета

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,10 +1,10 @@
 import UIKit
 
 extension UIColor {
-    static var ypBlack: IUColor { UIColor(named: "YP Black") ?? UIColor.black }
-    static var ypWhite: IUColor { UIColor(named: "YP White") ?? UIColor.white }
-    static var ypGreen: IUColor { UIColor(named: "YP Green") ?? UIColor.green }
-    static var ypRed: IUColor { UIColor(named: "YP Red") ?? UIColor.red }
-    static var ypGray: IUColor { UIColor(named: "YP Gray") ?? UIColor.gray }
-    static var ypBackground: IUColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
+    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
 }
